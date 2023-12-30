1b6be0000b456349adefc32397f64ae4bbcddd1a
tanja2003 <127766521+tanja2003@users.noreply.github.com>
2023-12-30 17:26:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var subLinks = document.getElementById('subLinks');
+var termineContainer = document.getElementById('termineContainer');
+
+function showSubLinks() {
+    subLinks.style.display = 'block';
+}
+
+function hideSubLinks() {
+    subLinks.style.display = 'none';
+}
